5014ed64c1c4d2469055bc5112d0cff71b28e4f6
김대훈(Kim daehun) <anonkorea4869@naver.com>
2023-11-28 01:11:36 +0900

    message

---
+++ frontend/js/message.js
@@ -0,0 +1,107 @@
+// Initialize an object to store messages for each user
+const chatMessagesByUser = {};
+function navigateToIndex() {
+    window.location.href = 'index.html';
+}
+function openChat(username) {
+    // Show the chat window
+    document.getElementById('chatWindow').style.display = 'flex';
+
+    // Load chat history, user details, etc.
+    document.getElementById('chatUsername').innerText = username;
+
+    // For simplicity, load messages from the object or create an empty array
+    const messages = chatMessagesByUser[username] || [];
+
+    // Display the messages in the chat window
+    const chatMessages = document.getElementById('chatMessages');
+    chatMessages.innerHTML = messages.map(message => `<p>${message}</p>`).join('');
+
+    // Update the dataset with the current username
+    document.getElementById('chatWindow').dataset.username = username;
+
+}
+
+function closeChat() {
+    // Hide the chat window
+    document.getElementById('chatWindow').style.display = 'none';
+}
+
+function sendMessage() {
+    // Get the input value and username
+    const messageInput = document.getElementById('messageInput');
+    const message = messageInput.value;
+    const username = document.getElementById('chatWindow').dataset.username;
+
+    // For simplicity, let's just log the message for now
+    console.log(`Sending message to ${username}: ${message}`);
+
+    // Store the message in the object for the current user
+    if (!chatMessagesByUser[username]) {
+        chatMessagesByUser[username] = [];
+    }
+    chatMessagesByUser[username].push(`You: ${message}`);
+
+    // Display the updated messages in the chat window
+    openChat(username);
+
+    // Clear the input field
+    messageInput.value = '';
+}
+// function sendToServer(username, message) {
+//     fetch('/api/sendMessage', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             username: username,
+//             message: message,
+//         }),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log('Success:', data);
+//         // Handle success response from the server, if needed
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//         // Handle error from the server, if needed
+//     });
+// }
+function createNewChat() {
+    const newUsername = prompt("Enter the nickname for the new chat:");
+
+    if (newUsername) {
+        // Create a new chat list item dynamically
+        const chatList = document.querySelector('.chatList');
+        const newChatListItem = document.createElement('div');
+        newChatListItem.className = 'chatListItem';
+        newChatListItem.innerHTML = `
+            <img src="default-user-image.jpg" alt="">
+            <div class="chatInfo">
+                <h3>${newUsername}</h3>
+                <p>No messages yet</p>
+            </div>
+        `;
+        newChatListItem.onclick = function () {
+            openChat(newUsername);
+        };
+
+        // Add the new chat list item to the chat list
+        chatList.appendChild(newChatListItem);
+
+        // Open the new chat window
+        openChat(newUsername);
+    }
+}
+
+document.getElementById('messageInput').addEventListener('keydown', function(event) {
+    // Check if the pressed key is Enter (key code 13)
+    if (event.key === 'Enter') {
+        // Prevent the default action (form submission, line break, etc.)
+        event.preventDefault();
+        // Execute the sendMessage function
+        sendMessage();
+    }
+});
---
+++ frontend/js/sidebar.js
@@ -0,0 +1,29 @@
+let home = document.getElementById('sidebar-home');
+home.addEventListener('click', function() {
+    location.href='/frontend/index.html';
+    // 다른 동작을 수행하려면 이곳에 코드를 추가하세요.
+});
+
+let search = document.getElementById('sidebar-search');
+search.addEventListener('click', function() {
+    alert("미완성")
+    // 다른 동작을 수행하려면 이곳에 코드를 추가하세요.
+});
+
+let message = document.getElementById('sidebar-message');
+message.addEventListener('click', function() {
+    location.href='/frontend/message.html';
+    // 다른 동작을 수행하려면 이곳에 코드를 추가하세요.
+});
+
+let profile = document.getElementById('sidebar-profile');
+profile.addEventListener('click', function() {
+    location.href='/frontend/profile.html';
+    // 다른 동작을 수행하려면 이곳에 코드를 추가하세요.
+});
+
+let more = document.getElementById('sidebar-more');
+more.addEventListener('click', function() {
+    alert("미완성")
+    // 다른 동작을 수행하려면 이곳에 코드를 추가하세요.
+});
